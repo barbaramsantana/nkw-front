eb4030032c6255f4590e9e3cc4272351d950d371
Bárbara Mota Santana <barbaramotabms@gmail.com>
2021-10-20 23:27:14 -0300

    deploy e favorito

---
+++ src/pages/Perfil/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ContainerForm = styled.div`
+background-color: #ffffff;
+justify-content: center;
+align-items: center;
+text-align: center;
+border-radius: 25px 25px 25px 25px;
+margin-left: 5rem;
+margin-right: 5rem;
+padding-left: 1rem;
+padding-right: 1rem;
+padding-bottom:1rem;
+padding-top:1rem;
+margin-bottom:1rem;
+margin-top:1rem;
+box-shadow: 0px 2px 4px 4px rgba(0, 0, 0, 0.25);
+
+`;
