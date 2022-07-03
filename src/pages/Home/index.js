99733483f9d23251258982cb78a0e2993f59cd09
anycarolinys <caroliny663@gmail.com>
2022-07-03 17:32:41 -0300

    Modal excluir desejo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,119 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import './styled.css';
+import imovel from '../../img/imovel-exemplo.jpg';
 
 const Home = () => {
     return (
-        <div>
+        <body>
             <Navbar></Navbar>
-        </div>
+            <div class="offset-lg-3 main-title col">
+                <h2>Para Você</h2>
+            </div>
+
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div className="col categories lead">
+                        <div class="categories-title text-center ">
+                            <label class="lead fw-bold">Suas Categorias</label>
+                        </div>
+
+                        <div class="list-group">
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Imóveis
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Carros
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Eletrônicos
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Roupas
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Eletrodomésticos
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Eletrodomésticos
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Eletrodomésticos
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Eletrodomésticos
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Eletrodomésticos
+                            </label>
+                            <label className="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value="" />
+                                Eletrodomésticos
+                            </label>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-3 m-2">
+                        <div class="card">
+                            <img src={imovel} class="card-img-top" alt="" />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 m-2">
+                        <div class="card">
+                            <img src={imovel} class="card-img-top" alt="" />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 m-2">
+                        <div class="card">
+                            <img src={imovel} class="card-img-top" alt="" />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 m-2">
+                        <div class="card">
+                            <img src={imovel} class="card-img-top" alt="" />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 m-2">
+                        <div class="card">
+                            <img src={imovel} class="card-img-top" alt="" />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </body >
     )
 }
 
--- src/pages/MeusPedidos/index.js
+++ src/pages/MeusPedidos/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Link, withRouter} from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import Cabecalho from '../../components/Cabecalho';
 import Lista from '../../components/Lista';
 import { Titulo1, ContainerForm, ContainerCaracteristicas, ContainerDesejo, ContainerText, TextoLink } from './styled.js';
@@ -10,120 +10,139 @@ import api from '../../services/api';
 import { getToken } from '../../Auth/index';
 import { getName } from '../../Auth/index';
 import axios from 'axios';
-import Navbar from '../../components/Navbar/index'; 
+import Navbar from '../../components/Navbar/index';
 
-function MeusPedidos(){
+function MeusPedidos() {
     const [items, setItems] = useState([]);
     const name = getName();
-    useEffect(()=>{
+    useEffect(() => {
         async function loadListDesejos() {
-            try{
+            try {
                 const token = getToken();
                 //console.log(token);
-                const response = await api.get('/desejos',{headers:{token:`Bearer ${token}`}});
+                const response = await api.get('/desejos', { headers: { token: `Bearer ${token}` } });
                 //const response = await api.get('/desejos');
-        
+
                 setItems(response.data.desejos);
                 //console.log(response);
-            } catch(err){
+            } catch (err) {
                 console.error(err);
             }
         }
         loadListDesejos();
-    },[]);
+    }, []);
     //const response = await api.post("/desejos");
-    async function excluirDesejo(val){
-        try{
+    async function excluirDesejo(val) {
+        try {
             const token = getToken();
-            const response = await api.delete(`/desejos/${val}`, {headers:{token:`Bearer ${token}`}});
+            const response = await api.delete(`/desejos/${val}`, { headers: { token: `Bearer ${token}` } });
             //console.log(response);
             window.location.reload();
 
-        } catch(err){
+        } catch (err) {
             console.error(err);
         }
     }
-    async function isFavorito(id, val){
-        if(val == 1){
-            try{
+    async function isFavorito(id, val) {
+        if (val === 1) {
+            try {
                 const token = getToken();
-                const data = {"favorito": 0};
-                const response = await api.put(`/desejos/favorito/${id}`, data,{headers:{token:`Bearer ${token}`}});
+                const data = { "favorito": 0 };
+                const response = await api.put(`/desejos/favorito/${id}`, data, { headers: { token: `Bearer ${token}` } });
                 //console.log(response);
                 window.location.reload();
-    
-            } catch(err){
+
+            } catch (err) {
                 console.error(err);
-            } 
-        }else{
-            try{
+            }
+        } else {
+            try {
                 const token = getToken();
-                const data = {"favorito": 1};
-                const response = await api.put(`/desejos/favorito/${id}`, data,{headers:{token:`Bearer ${token}`}});
+                const data = { "favorito": 1 };
+                const response = await api.put(`/desejos/favorito/${id}`, data, { headers: { token: `Bearer ${token}` } });
                 //console.log(response);
                 window.location.reload();
-    
-            } catch(err){
+
+            } catch (err) {
                 console.error(err);
-            }  
+            }
         }
     }
-    return(
-        <div>
-            {//<Cabecalho></Cabecalho>
-            <Navbar></Navbar>
-        }
-        <br />
-            <div className="meus-desejos">
-                <Titulo1>Desejos</Titulo1>
+    return (
+        <body>
+            <div>
+                {//<Cabecalho></Cabecalho>
+                    <Navbar></Navbar>
+                }
                 <br />
-                <div className="group-my-desejos">
-                   { /*<Link className="link-entrar" to="/registerdesejos" style={{ textDecoration: 'none' }}>
+                <div className="meus-desejos">
+                    <Titulo1>Desejos</Titulo1>
+                    <br />
+                    <div className="group-my-desejos">
+                        { /*<Link className="link-entrar" to="/registerdesejos" style={{ textDecoration: 'none' }}>
                         <h2 className="new-desejo"><AiFillPlusCircle className="add-circle"/> Novo Desejo</h2>
                     </Link>*/
-}
+                        }
+                    </div>
                 </div>
+                {//<Lista desejos={items}></Lista>
+                    <ContainerForm>
+                        <div className="container">
+                            <div className="row">
+                                {Object.keys(items).map(item => (
+                                    //<ContainerDesejo>
+                                    <ContainerDesejo className="col-10 col-lg-4 com-md-6">
+                                        <div className="col">
+                                            <Link style={{ textDecoration: 'none' }} className="link-desejo" to={`/desejo/${items[item]._id}`}>
+                                                <div >
+                                                    {
+                                                        <TextoLink>{name}</TextoLink>
+                                                    }
+                                                </div>
+                                                <ContainerText>{items[item].descricao
+                                                }
+                                                </ContainerText>
+                                            </Link>
+                                            <div id="icones-fav">
+                                                {/*isFavorito(items[item].favorito)*/}
+                                                {items[item].favorito === 1 && (
+                                                    <AiFillHeart size="1.5rem" color="#BB6BD9" onClick={async () => { await isFavorito(items[item]._id, items[item].favorito) }} style={{ cursor: "pointer" }} />
+                                                )}
+                                                {items[item].favorito === 0 && (
+                                                    <AiOutlineHeart size="1.5rem" color="#BB6BD9" onClick={async () => { await isFavorito(items[item]._id, items[item].favorito) }} style={{ cursor: "pointer" }} />
+                                                )}
+                                                <BiTrash size="1.5rem" color="#BB6BD9" data-bs-toggle="modal" data-bs-target="#confirmation"/>
+
+                                                {/* <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#confirmation">
+                                                    Launch demo modal
+                                                </button> */}
+
+                                                <div class="modal fade" id="confirmation" tabindex="-1" aria-labelledby="confirmationLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="confirmationLabel">Tem certeza que deseja excluir este desejo?</h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                                                <button type="button" class="btn btn-primary" onClick={async () => { await excluirDesejo(items[item]._id)}} >Sim</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </ContainerDesejo>
+                                ))}
+                            </div>
+                        </div>
+                    </ContainerForm>
+
+                }
             </div>
-            {//<Lista desejos={items}></Lista>
-            <ContainerForm>
-            <div className="container">
-                <div className="row">
-                {Object.keys(items).map( item => (
-                    //<ContainerDesejo>
-                    <ContainerDesejo className="col-10 col-lg-4 com-md-6">
-                        <div className="col">
 
-
-                <Link style={{ textDecoration: 'none' }} className="link-desejo" to={`/desejo/${items[item]._id}`}>
-                    <div >
-                        {//Bárbara M. S.
-                        <TextoLink>{name}</TextoLink>
-                    }
-                    </div>
-                    <ContainerText>{items[item].descricao
-                    }
-                    </ContainerText>
-                </Link>
-                    <div id="icones-fav">
-                        {/*isFavorito(items[item].favorito)*/}
-                        {items[item].favorito == 1 && ( 
-                            <AiFillHeart size="1.5rem" color="#BB6BD9" onClick={async () =>{await isFavorito(items[item]._id, items[item].favorito)}}/>
-                        )}
-                        {items[item].favorito == 0 && (
-                            <AiOutlineHeart size="1.5rem" color="#BB6BD9" onClick={async () =>{await isFavorito(items[item]._id, items[item].favorito)}}/>
-                        )}
-                        <BiTrash size="1.5rem" color="#BB6BD9" onClick={async () =>{await excluirDesejo(items[item]._id)}}/>
-                    </div>
-                    </div>
-                    </ContainerDesejo>
-                ))}
-                </div>
-
-            </div>
-            </ContainerForm>
-            }
-
-        </div>
+        </body>
     )
 }
 export default MeusPedidos;
